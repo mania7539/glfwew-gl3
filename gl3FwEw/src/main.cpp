7da23ced69bafb0424f3de2fe53f8540f332fd5a
David.Rz.Liu <mania7539@gmail.com>
2018-01-19 02:15:30 +0800

    L5: Vertex Attributes and Layouts in OpenGL

--- gl3FwEw/src/main.cpp
+++ gl3FwEw/src/main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -21,36 +21,60 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	unsigned int a;
-	glGenBuffers(1, &a);
-
-	// TODO: GLEW code
-	// Documentation: We need to create a valid OpenGL rendering context
-	// where do we do this? glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) {
-		std::cout << "Failed to initialize GLEW" << std::endl;
-		return -1;
-	}
+		std::cout << "Error!" << std::endl;
+	};
 
-	std::cout << "GLVERSION: " << glGetString(GL_VERSION) << std::endl;
-	// output: GLVERSION: 2.1 INTEL-10.30.12
-	//                    ^GL ^Driver provider software version (on My Mac)
-	// output: GLVERSION: 4.2.0 Build 10.18.10.3643
-	//                    ^GL   ^Driver provider software version(on My Windows8.1)
+	std::cout << "GL_VERSION: " << glGetString(GL_VERSION);
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	}; // This is vertex positions of our triangle
+	   // But a "vetex" doesn't only contain position, it can contain more of that
+	   // : a position is an attribute, a color is an attribute, a texture coordinate is an attribute
+	   // : a normal is an attribute
+	   // NOTE: VertexAttribPointer - Attribute is a part which you can deal with your self or managed by opengl by default
+	   // : stride - Specifies the byte offset between consecutive generic vertex attributes.
+	   //				(: it's the amount of bytes between each vertex, or the size of each vertex)
+	   //				you'll get weird artifacts in your rendering or just black screen while you just get only 1 byte out of place
 
-	/* Loop until the user closes the window */
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);				// Generate/Create a GL Buffer, we should provide an Integer as a memory which we can write into 
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);	// How do I want to use the GL Buffer? Define it to a specific buffer:
+											//	target - GL_ARRAY_BUFFER: it's just an array
+											//	buffer - an integer comes from memory
+											// if we use glBindBuffer(GL_ARRAY_BUFFER, 0); then GPU won't draw the triangle out since we bind something else
+
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	// above: Set data which we want to use to the specific GPU buffer
+	// : STATIC, DYNAMIC: we should let GPU knows that if the buffer can be modified more than ONCE.
+	// : DRAW: we want to draw things with the buffer, so use it
+
+	glEnableVertexAttribArray(0);	// index: it's the index we want to enable attribute.
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	// we need to tell opengl how we layout the data with - glVertexAttribPointer
+	// example: glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*) 8);
+	// : normal: GL_FALSE - if we want them to be normalized
+	// : pass 4*2 = 8 as offset (in byte) to the texture coordinate - converts to a pointer with (const void*)
+	// IMPORTANT: we need to enable attribute feature with - glEnableVertexAttribArray
+
+	/*
+	* Loop until the user closes the window:
+	* : This is like onDraw, so it will draw on each frame when updating,
+	* : but we don't have to put the buffer code here, but draw command only
+	*/
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// START: legacy opengl
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
-		// END: legacy opengl
+		// TODO: modern gl codes begin:
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// above: glDrawArrays ¡X render primitives from array data
+		// : first - 0 means the index of the array we want to start
+		// : count - the number of axis (x, y) of the array we want to draw
+		// NOTE: similar API - glDrawElements(GL_TRIANGLES, 3, UNSIGNED_INT, ...) used with index buffer
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
